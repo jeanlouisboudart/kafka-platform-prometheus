f5f80d3a8f92101bab340077c03ce31c5b932dfd
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2019-08-19 20:01:26 +0200

    kickstart producer/consumer jmx collect

---
+++ sample-application/consumer/src/main/java/com/sample/SimpleConsumer.java
@@ -0,0 +1,29 @@
+package com.sample;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "simple-consumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        System.out.println("Subscribing to `sample` topic");
+        consumer.subscribe(Arrays.asList("sample"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(400));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Received offset = " + record.offset() + ", key = " + record.key() + ", value = " + record.value());
+            }
+        }
+    }
+}
---
+++ sample-application/producer/src/main/java/com/sample/SimpleProducer.java
@@ -0,0 +1,31 @@
+package com.sample;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleProducer {
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        System.out.println("Sending data to `sample` topic");
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            int i = 0;
+            while (true) {
+                ProducerRecord<String, String> record = new ProducerRecord<>("sample", "key " + i, "Value " + i);
+                System.out.println("Sending " + record.key() + " " + record.value());
+                producer.send(record);
+                i++;
+                TimeUnit.SECONDS.sleep(1);
+            }
+        }
+    }
+}
