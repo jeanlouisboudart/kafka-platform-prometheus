1c266238e9fc6d2d3422a8ebc14a9eed8103e2d9
ksilin <konstantin.silin@gmail.com>
2019-11-13 14:52:02 +0100

    sync production, sync offset commit. long keys

--- sample-application/consumer/src/main/java/com/sample/SimpleConsumer.java
+++ sample-application/consumer/src/main/java/com/sample/SimpleConsumer.java
@@ -14,15 +14,37 @@ public class SimpleConsumer {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "simple-consumer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "300000"); // 300000 = 5 min by default
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.LongDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<Long, String> consumer = new KafkaConsumer<>(props);
         System.out.println("Subscribing to `sample` topic");
         consumer.subscribe(Arrays.asList("sample"));
+        long lastKey = 0L;
         while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(400));
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Received offset = " + record.offset() + ", key = " + record.key() + ", value = " + record.value());
+            ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty() && records.count() != 1) {
+                System.out.println("Received " + records.count());
+            }
+            for (ConsumerRecord<Long, String> record : records) {
+
+                if (record.key() - lastKey != 1L) {
+                    System.out.println("---");
+                    System.out.println("KEY GAP FROM " + lastKey + " to " + record.key());
+                }
+                lastKey = record.key();
+
+                System.out.println("Received offset = " + record.offset() + ", key = " + record.key() + ", value = "
+                        + record.value());
+
+            }
+            try {
+                consumer.commitSync();
+            } catch (Exception e) {
+                System.out.println("failed to commit: " + e.getMessage());
             }
         }
     }
--- sample-application/producer/src/main/java/com/sample/SimpleProducer.java
+++ sample-application/producer/src/main/java/com/sample/SimpleProducer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -14,15 +15,22 @@ public class SimpleProducer {
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer");
         System.out.println("Sending data to `sample` topic");
-        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-            int i = 0;
+        try (Producer<Long, String> producer = new KafkaProducer<>(props)) {
+            long i = 0;
             while (true) {
-                ProducerRecord<String, String> record = new ProducerRecord<>("sample", "key " + i, "Value " + i);
+                ProducerRecord<Long, String> record = new ProducerRecord<>("sample", i, "Value " + i);
                 System.out.println("Sending " + record.key() + " " + record.value());
-                producer.send(record);
+                try {
+                    RecordMetadata rm = producer.send(record).get();
+                    System.out.println("success. offset: " + rm.offset());
+                } catch (Exception e) {
+                    System.out.println("failed sending " + record.key() + ": " + e.getMessage());
+                }
+
                 i++;
                 TimeUnit.SECONDS.sleep(1);
             }
