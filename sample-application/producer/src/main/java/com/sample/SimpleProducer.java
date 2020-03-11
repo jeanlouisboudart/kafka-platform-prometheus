ac22cd620d06a63066122f04a79f1767cee8b39b
ksilin <konstantin.silin@gmail.com>
2020-03-11 20:21:55 +0100

    env properties replace coded properties in Producer

--- sample-application/producer/src/main/java/com/sample/SimpleProducer.java
+++ sample-application/producer/src/main/java/com/sample/SimpleProducer.java
@@ -7,53 +7,72 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class SimpleProducer {
+
+    private static final String KAFKA_ENV_PREFIX = "KAFKA_";
+
     public static void main(String[] args) throws InterruptedException {
 
         String topicName = (args.length == 0 ? "workshop_topic_1" : args[0]);
-
-        System.out.println("started with args");
+        System.out.println("producer args:");
         for (String a : args) {
             System.out.println(a);
-    }
+        }
 
-        Properties props = new Properties();
+        Properties props = buildProperties(defaultProps, System.getenv(), KAFKA_ENV_PREFIX);
+        System.out.println("creating producer with props:");
+        props.forEach( (k, v) -> System.out.println(k + ": " + v));
 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092");
-        props.put(ProducerConfig.ACKS_CONFIG, "all"); // default: "1"
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer");
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // default: 5
-        props.put(ProducerConfig.RETRIES_CONFIG, "10"); // default: Integer.MAX_VALUE
-        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "900"); // default: 30000
-        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "1000"); // default: 120000
-        // props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "prod-1");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringSerializer");
-        System.out.println("Sending data to"+ topicName +  "topic");
+        System.out.println("Sending data to" + topicName + "topic");
         try (Producer<Long, String> producer = new KafkaProducer<>(props)) {
 
             // init TX, otherwise sending will not work with idempotency
             // Cannot perform a 'send' before completing a call to initTransactions when transactions are enabled
             // producer.initTransactions();
 
-            long i = 0;
+            long id = 0;
             while (true) {
-                ProducerRecord<Long, String> record = new ProducerRecord<>(topicName, i, "Value " + i);
+                ProducerRecord<Long, String> record = new ProducerRecord<>(topicName, id, "Value " + id);
                 LocalDateTime now = LocalDateTime.now();
-                System.out.println( now + "Sending " + record.key() + " " + record.value() + topicName);
+                System.out.println(now + "Sending " + record.key() + " " + record.value() + topicName);
                 try {
                     RecordMetadata rm = producer.send(record).get(100, TimeUnit.MILLISECONDS);
-                    System.out.println("success. offset: " + rm.offset());
+                    System.out.println("succeeded sending. offset: " + rm.offset());
                 } catch (Exception e) {
                     System.out.println(now + "failed sending " + record.key() + ": " + e.getMessage());
                 }
-                i++;
+                id++;
                 TimeUnit.MILLISECONDS.sleep(1);
             }
         }
     }
+
+    private static Map<String, String> defaultProps = Map.of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9092,kafka-3:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer",
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+    private static Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
+        Map<String, String> systemProperties = envProps.entrySet()
+                .stream()
+                .filter(e -> e.getKey().startsWith(prefix))
+                .collect(Collectors.toMap(
+                        e -> e.getKey()
+                                .replace(prefix, "")
+                                .toLowerCase()
+                                .replace("_", ".")
+                        , e -> e.getValue())
+                );
+
+        Properties props = new Properties();
+        props.putAll(baseProps);
+        props.putAll(systemProperties);
+        return props;
+    }
+
 }
