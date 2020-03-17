b3d7d1f9fb46d8b17042a7c2c07c830aad8adfc8
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2020-03-17 09:27:28 +0100

    refactor producer : * auto creation of topic * backoff between messages configurable via env property * replication factor configurable via env property * nb partitions configurable via env property

--- sample-application/producer/src/main/java/com/sample/SimpleProducer.java
+++ sample-application/producer/src/main/java/com/sample/SimpleProducer.java
@@ -1,63 +1,85 @@
 package com.sample;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.errors.TopicExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class SimpleProducer {
 
     private static final String KAFKA_ENV_PREFIX = "KAFKA_";
+    private final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final Properties properties;
+    private final String topicName;
+    private final Long messageBackOff;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SimpleProducer simpleProducer = new SimpleProducer();
+        simpleProducer.start();
+    }
 
-        String topicName = (args.length == 0 ? "workshop_topic_1" : args[0]);
-        System.out.println("producer args:");
-        for (String a : args) {
-            System.out.println(a);
-        }
+    public SimpleProducer() throws ExecutionException, InterruptedException {
+        properties = buildProperties(defaultProps, System.getenv(), KAFKA_ENV_PREFIX);
+        topicName = System.getenv().getOrDefault("TOPIC","sample");
+        messageBackOff = Long.valueOf(System.getenv().getOrDefault("MESSAGE_BACKOFF","100"));
 
-        Properties props = buildProperties(defaultProps, System.getenv(), KAFKA_ENV_PREFIX);
-        System.out.println("creating producer with props:");
-        props.forEach( (k, v) -> System.out.println(k + ": " + v));
+        final Integer nbPartitions =  Integer.valueOf(System.getenv().getOrDefault("NB_PARTITIONS","2"));
+        final Short replicationFactor =  Short.valueOf(System.getenv().getOrDefault("REPLICATION_FACTOR","1"));
 
-        System.out.println("Sending data to topic " + topicName);
-        try (Producer<Long, String> producer = new KafkaProducer<>(props)) {
+        AdminClient adminClient = KafkaAdminClient.create(properties);
+        createTopic(adminClient, topicName, nbPartitions, replicationFactor);
+    }
 
-            // init TX, otherwise sending will not work with idempotency
-            // Cannot perform a 'send' before completing a call to initTransactions when transactions are enabled
-            // producer.initTransactions();
+    private void start() throws InterruptedException {
+        logger.info("creating producer with props: {}", properties);
+        properties.forEach( (k, v) -> System.out.println(k + ": " + v));
+
+
+        logger.info("Sending data to `{}` topic", topicName);
+        try (Producer<Long, String> producer = new KafkaProducer<>(properties)) {
 
             long id = 0;
             while (true) {
                 ProducerRecord<Long, String> record = new ProducerRecord<>(topicName, id, "Value " + id);
                 LocalDateTime now = LocalDateTime.now();
-                System.out.println(now + "Sending " + record.key() + " " + record.value() + " to " + topicName);
-                try {
-                    RecordMetadata rm = producer.send(record).get(100, TimeUnit.MILLISECONDS);
-                    System.out.println("succeeded sending. offset: " + rm.offset());
-                } catch (Exception e) {
-                    System.out.println(now + "failed sending " + record.key() + ": " + e.getMessage());
-                }
+                logger.info("Sending Key = {}, Value = {}", record.key(), record.value());
+                producer.send(record,(recordMetadata, exception) -> sendCallback(record, recordMetadata,exception));
                 id++;
-                TimeUnit.MILLISECONDS.sleep(1);
+                TimeUnit.MILLISECONDS.sleep(messageBackOff);
             }
         }
     }
 
-    private static Map<String, String> defaultProps = Map.of(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092,kafka-2:9092,kafka-3:9092",
+    private void sendCallback(ProducerRecord<Long, String> record, RecordMetadata recordMetadata, Exception e) {
+        if (e == null) {
+            logger.debug("succeeded sending. offset: {}", recordMetadata.offset());
+        } else {
+            logger.error("failed sending key: {}" + record.key(), e);
+        }
+    }
+
+    private Map<String, String> defaultProps = Map.of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092",
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer",
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-    private static Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
+    private Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
         Map<String, String> systemProperties = envProps.entrySet()
                 .stream()
                 .filter(e -> e.getKey().startsWith(prefix))
@@ -73,6 +95,19 @@ public class SimpleProducer {
         props.putAll(baseProps);
         props.putAll(systemProperties);
         return props;
+    }
+
+    private void createTopic(AdminClient adminClient, String topicName, Integer nbPartitions, Short replicationFactor) throws InterruptedException, ExecutionException {
+        if (!adminClient.listTopics().names().get().contains(topicName)) {
+            logger.info("Creating topic {}", topicName);
+            final NewTopic newTopic = new NewTopic(topicName, nbPartitions, replicationFactor);
+            try {
+                CreateTopicsResult topicsCreationResult = adminClient.createTopics(Collections.singleton(newTopic));
+                topicsCreationResult.all().get();
+            } catch (TopicExistsException e) {
+                //silent ignore if topic already exists
+            }
+        }
     }
 
 }
