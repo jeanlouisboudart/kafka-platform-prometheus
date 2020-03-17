54f46603176b06d149a8eaf612a75f5ad0d85e2d
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2020-03-17 10:29:29 +0100

    refactor consumer : * use proper logging * configurable commit strategy

--- sample-application/consumer/src/main/java/com/sample/SimpleConsumer.java
+++ sample-application/consumer/src/main/java/com/sample/SimpleConsumer.java
@@ -1,11 +1,11 @@
 package com.sample;
 
-import org.apache.commons.cli.*;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Properties;
@@ -15,75 +15,90 @@ import java.util.stream.Collectors;
 public class SimpleConsumer {
 
     private static final String KAFKA_ENV_PREFIX = "KAFKA_";
+    private final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    private final Properties properties;
+    private final String topicName;
+    private final boolean checkGaps;
+    private final CommitStrategy commitStrategy;
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
+        SimpleConsumer simpleConsumer = new SimpleConsumer();
+        simpleConsumer.start();
+    }
 
-        SimpleConsumerOptions opts = new SimpleConsumerOptions(args);
+    public SimpleConsumer() {
+        properties = buildProperties(defaultProps, System.getenv(), KAFKA_ENV_PREFIX);
+        topicName = System.getenv().getOrDefault("TOPIC","sample");
+        checkGaps = Boolean.valueOf(System.getenv().getOrDefault("CHECK_GAPS","false"));
+        commitStrategy = CommitStrategy.valueOf(System.getenv().getOrDefault("COMMIT_STRATEGY","AUTO_COMMIT"));
+    }
+
+    private void start() {
 
         Properties props = buildProperties(defaultProps, System.getenv(), KAFKA_ENV_PREFIX);
-        System.out.println("creating consumer with props:");
-        props.forEach( (k, v) -> System.out.println(k + ": " + v));
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, commitStrategy == CommitStrategy.AUTO_COMMIT);
+        logger.info("creating producer with props: {}", properties);
 
         KafkaConsumer<Long, String> consumer = new KafkaConsumer<>(props);
 
-        System.out.println("Subscribing to " + opts.topicName + " prefix");
-        consumer.subscribe(Pattern.compile(opts.topicName), listener);
+        logger.info("Subscribing to {} prefix", topicName);
+        consumer.subscribe(Pattern.compile(topicName), listener);
         long lastKey = 0L;
         while (true) {
             ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis(100));
 
-            LocalDateTime now = LocalDateTime.now();
-
-            if (opts.verbose && !records.isEmpty() && records.count() != 1) {
-                System.out.println(now + " Received " + records.count());
+            if (logger.isDebugEnabled() && !records.isEmpty()) {
+                logger.debug("Received {}", records.count());
             }
             for (ConsumerRecord<Long, String> record : records) {
                 String rp = record.topic() + "#" + record.partition();
 
-                if (opts.checkGaps && record.key() - lastKey != 1L) {
-                    System.out.println("---");
-                    System.out.println(now + " KEY GAP on " + rp + "FROM " + lastKey + " to " + record.key());
+                if (checkGaps && record.key() - lastKey != 1L) {
+                    logger.warn("KEY GAP found on {} from {} to {}",rp, lastKey,record.key());
                 }
                 lastKey = record.key();
+                logger.debug("Received {} offset = {}, key = {} , value = {}", rp, record.offset(), record.key(), record.value());
 
-                if (opts.verbose)
-                    System.out.println(now + " Received " + rp + " offset = " + record.offset() + ", key = "
-                            + record.key() + ", value = " + record.value());
+                if (commitStrategy == CommitStrategy.PER_MESSAGE) {
+                    commit(consumer);
+                }
             }
-            try {
-                consumer.commitSync();
-            } catch (Exception e) {
-                System.out.println(now + "failed to commit: " + e.getMessage());
+            if (commitStrategy == CommitStrategy.PER_BATCH) {
+                commit(consumer);
             }
         }
     }
 
+    private void commit(KafkaConsumer<Long, String> consumer) {
+        try {
+            consumer.commitSync();
+        } catch (Exception e) {
+            logger.error("failed to commit: ", e);
+        }
+    }
+
     private static ConsumerRebalanceListener listener = new ConsumerRebalanceListener() {
+        private final Logger logger = LoggerFactory.getLogger(ConsumerRebalanceListener.class);
+
 
         @Override
         public void onPartitionsRevoked(Collection<TopicPartition> revokedPartitions) {
-            System.out.println("rebalance: revoked " + revokedPartitions.size() + " partitions");
-            for (TopicPartition p : revokedPartitions) {
-                System.out.println("rebalance: revoked  " + p.topic() + ":" + p.partition());
-            }
+            logger.info("Partition revoked : {}", revokedPartitions);
         }
 
         @Override
         public void onPartitionsAssigned(Collection<TopicPartition> assignedPartitions) {
-            System.out.println("rebalance: assigned " + assignedPartitions.size() + " partitions");
-            for (TopicPartition p : assignedPartitions) {
-                System.out.println("rebalance: assigned  " + p.topic() + ":" + p.partition());
-            }
+            logger.info("Partition assigned : {}", assignedPartitions);
         }
     };
 
-    private static Map<String, String> defaultProps = Map.of(
-            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092,kafka-2:9092,kafka-3:9092",
+    private Map<String, String> defaultProps = Map.of(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092",
             ConsumerConfig.GROUP_ID_CONFIG, "simple-consumer",
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer",
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
-    private static Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
+    private Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
         Map<String, String> systemProperties = envProps.entrySet()
                 .stream()
                 .filter(e -> e.getKey().startsWith(prefix))
@@ -99,5 +114,11 @@ public class SimpleConsumer {
         props.putAll(baseProps);
         props.putAll(systemProperties);
         return props;
+    }
+
+    public enum CommitStrategy {
+        AUTO_COMMIT,
+        PER_MESSAGE,
+        PER_BATCH;
     }
 }
