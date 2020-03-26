11bcadf675422fb62e3bfa55ff3298e345004307
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2020-03-26 15:20:44 +0100

    Streams: * default replication factor should be 3 * print topology

--- sample-application/streams/src/main/java/com/sample/SimpleKafkaStreams.java
+++ sample-application/streams/src/main/java/com/sample/SimpleKafkaStreams.java
@@ -8,9 +8,12 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,7 +43,7 @@ public class SimpleKafkaStreams {
         outputTopic = System.getenv().getOrDefault("OUTPUT_TOPIC","output");
 
         final Integer numberOfPartitions =  Integer.valueOf(System.getenv().getOrDefault("NUMBER_OF_PARTITIONS","2"));
-        final Short replicationFactor =  Short.valueOf(System.getenv().getOrDefault("REPLICATION_FACTOR","1"));
+        final Short replicationFactor =  Short.valueOf(System.getenv().getOrDefault("REPLICATION_FACTOR","3"));
 
         AdminClient adminClient = KafkaAdminClient.create(properties);
         createTopic(adminClient, inputTopic, numberOfPartitions, replicationFactor);
@@ -55,11 +58,16 @@ public class SimpleKafkaStreams {
         KTable<String, Long> count = builder.stream(inputTopic, Consumed.with(Serdes.Long(), Serdes.String()))
                 .groupBy((key, value) -> key % 2 == 0 ? "even" : "odd", Grouped.with(Serdes.String(), Serdes.String()))
                 .count();
-        count
-                .toStream()
-                .to(outputTopic, Produced.with(Serdes.String(),Serdes.Long()));
 
-        KafkaStreams streams = new KafkaStreams(builder.build(),properties);
+        KStream<String, Long> stream = count
+                .toStream();
+
+        stream.print(Printed.toSysOut());
+        stream.to(outputTopic, Produced.with(Serdes.String(),Serdes.Long()));
+        Topology topology = builder.build();
+        logger.info(topology.describe().toString());
+
+        KafkaStreams streams = new KafkaStreams(topology,properties);
         streams.start();
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
@@ -67,7 +75,8 @@ public class SimpleKafkaStreams {
 
     private Map<String, String> defaultProps = Map.of(
             StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092,localhost:19093,localhost:19094",
-            StreamsConfig.APPLICATION_ID_CONFIG, System.getenv().getOrDefault("APPLICATION_ID","sample-streams")
+            StreamsConfig.APPLICATION_ID_CONFIG, System.getenv().getOrDefault("APPLICATION_ID","sample-streams"),
+            StreamsConfig.REPLICATION_FACTOR_CONFIG, "3"
             );
 
     private Properties buildProperties(Map<String, String> baseProps, Map<String, String> envProps, String prefix) {
